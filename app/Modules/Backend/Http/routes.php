0334cfc02cbd2ea4e4a5dc06db245596adfd101b
wuyu <492444775@qq.com>
2019-10-27 17:15:51 +0800

    修改Auth中间件

--- app/Modules/Backend/Http/routes.php
+++ app/Modules/Backend/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', ['as'=>'index', 'uses'=>'IndexController@index']);
+Route::get('/', ['middleware' => ['auth'], 'as'=>'index', 'uses'=>'IndexController@index']);
 Route::get('/login', ['as'=>'login_page', 'uses'=>'IndexController@loginPage']);
 Route::post('/login', ['as'=>'login', 'uses'=>'IndexController@login']);

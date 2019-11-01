fa79235da669b561bae5a0cc136254c83b9ca05e
wuyu <492444775@qq.com>
2019-11-01 23:06:01 +0800

    系统设置页面

--- app/Modules/Backend/Resources/Views/system/config.blade.php
+++ app/Modules/Backend/Resources/Views/system/config.blade.php
@@ -3,6 +3,36 @@
     <fieldset class="layui-elem-field layui-field-title" style="margin-top: 20px;">
         <legend>系统设置</legend>
     </fieldset>
+    <div class="row">
+        <div class="layui-col-xs6">
+            <form class="layui-form" action="">
+                <div class="layui-form-item">
+                    <label class="layui-form-label">博客标题</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="title" placeholder="" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">关键字</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="keywords" placeholder="" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-form-item layui-form-text">
+                    <label class="layui-form-label">描述</label>
+                    <div class="layui-input-block">
+                        <textarea name="desc" placeholder="" class="layui-textarea"></textarea>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <div class="layui-input-block">
+                        <button class="layui-btn">立即提交</button>
+                        <button type="reset" class="layui-btn layui-btn-primary">重置</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
 @section('scripts')
     <script>

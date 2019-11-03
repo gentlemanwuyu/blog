d923648c0f96429c30c451137ddb586002e83da8
wuyu <492444775@qq.com>
2019-11-03 09:32:26 +0800

    添加tinymce汉化包

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ elixir(function(mix) {
     mix.copy('node_modules/jstree/dist','public/assets/jstree/dist');
     mix.copy('node_modules/jquery/dist','public/assets/jquery/dist');
     mix.copy('node_modules/tinymce','public/assets/tinymce');
+    mix.copy('resources/assets/tinymce/langs','public/assets/tinymce/langs');
     mix.copy('resources/assets/js','public/assets/js');
     mix.copy('resources/assets/css','public/assets/css');
     mix.copy('resources/assets/img','public/assets/img');

941301bf2960b40bad9472f8ab5d61cb6c3f8138
wuyu <492444775@qq.com>
2019-11-05 23:56:47 +0800

    发布文章页面增加摘要图片

--- app/Modules/Backend/Resources/Views/article/create_or_update_article.blade.php
+++ app/Modules/Backend/Resources/Views/article/create_or_update_article.blade.php
@@ -1,4 +1,22 @@
 @extends('backend::layouts.default')
+@section('css')
+    <style>
+        .img_container {
+            width: 212px!important;
+            height: 212px;
+            border: 1px dashed #8D8D8D;
+            padding: 5px;
+            border-radius: 3px;
+            display: table-cell;
+            vertical-align: middle;
+            text-align: center;
+        }
+        .img_container>img {
+            width: auto;
+            height: 200px;
+        }
+    </style>
+@endsection
 @section('content')
     <fieldset class="layui-elem-field layui-field-title" style="margin-top: 20px;">
         <legend>发布文章</legend>
@@ -27,6 +45,21 @@
             <label class="layui-form-label">摘要</label>
             <div class="layui-input-block">
                 <textarea name="summary" placeholder="" class="layui-textarea">{{$article['summary'] or ''}}</textarea>
+            </div>
+        </div>
+        <div class="layui-form-item layui-form-text">
+            <input type="hidden" name="summary_image">
+            <label class="layui-form-label">摘要图片</label>
+            <div class="layui-input-block">
+                <button type="button" class="layui-btn" id="upload_img">上传图片</button>
+                <button type="button" class="layui-btn">从图库中选择</button>
+            </div>
+        </div>
+        <div class="layui-form-item layui-form-text" style="display: none;">
+            <div class="layui-input-block">
+                <div class="img_container">
+
+                </div>
             </div>
         </div>
         <div class="layui-form-item">
@@ -113,8 +146,9 @@
             }
         });
 
-        layui.use(['form'], function () {
-            var form = layui.form;
+        layui.use(['form', 'upload'], function () {
+            var form = layui.form
+            ,upload = layui.upload;
 
             form.on('submit(article)', function(data){
                 var form_data = data.field;
@@ -142,6 +176,37 @@
                 });
 
                 return false;
+            });
+
+            var uploadInst = upload.render({
+                elem: '#upload_img' //绑定元素
+                ,url: '{{route('admin::article.upload')}}' //上传接口
+                ,accept: 'images'
+                ,headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+                ,field: 'image'
+                ,before: function(obj){
+                    layer.load();
+                }
+                ,done: function(res, index, upload){
+                    layer.closeAll('loading');
+                    if (res.status) {
+                        $('.img_container').html('');
+                        $('.img_container').append('<img src="' + res.url + '">');
+                        $('.img_container').parents('div.layui-form-item').show();
+                        $('input[name=summary_image]').val(res.url);
+                    }else {
+                        layer.msg("图片上传失败", {icon:2});
+                        $('.img_container').html('');
+                        $('.img_container').parents('div.layui-form-item').hide();
+                        $('input[name=summary_image]').val('');
+                    }
+                }
+                ,error: function(index, upload){
+                    layer.closeAll('loading');
+                    $('.img_container').html('');
+                    $('.img_container').parents('div.layui-form-item').hide();
+                    $('input[name=summary_image]').val('');
+                }
             });
         });
     </script>

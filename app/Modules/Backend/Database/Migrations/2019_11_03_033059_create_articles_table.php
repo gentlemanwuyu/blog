1b9aaf2612955191431d126d317ac2f6b36e21bd
WuYu <yu.wu@patpat.com>
2019-11-07 14:02:30 +0800

    article表增加摘要图等字段

--- app/Modules/Backend/Database/Migrations/2019_11_03_033059_create_articles_table.php
+++ app/Modules/Backend/Database/Migrations/2019_11_03_033059_create_articles_table.php
@@ -18,6 +18,9 @@ class CreateArticlesTable extends Migration
 			$table->string('keywords', 1024)->default('')->comment('关键词');
 			$table->text('content')->default('')->comment('内容');
 			$table->string('summary', 1024)->default('')->comment('摘要');
+			$table->integer('summary_image_id')->default(0)->comment('摘要图片ID');
+			$table->string('summary_image_url', 1024)->default('')->comment('摘要图片url');
+			$table->string('summary_image_desc')->default('')->comment('摘要图片描述');
 			$table->integer('category_id')->default(0)->comment('分类ID');
 			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('创建时间');
 			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('最后更新时间');

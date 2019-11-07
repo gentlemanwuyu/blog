8907c3a8bd74d34b43e699266ae88ff4762e116e
WuYu <yu.wu@patpat.com>
2019-11-07 14:58:47 +0800

    fix

--- app/Modules/Backend/Database/Migrations/2019_11_02_065314_create_system_configs_table.php
+++ app/Modules/Backend/Database/Migrations/2019_11_02_065314_create_system_configs_table.php
@@ -15,7 +15,7 @@ class CreateSystemConfigsTable extends Migration
 		Schema::create('system_configs', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('name')->default('')->comment('配置名');
-			$table->string('value', 1024)->default('')->comment('配置值');
+			$table->text('value')->default('')->comment('配置值');
 			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('创建时间');
 			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('最后更新时间');
 			$table->unique('name');

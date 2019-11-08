efcf483608c9ef231545cea7ab7d692fa74f0d42
WuYu <yu.wu@patpat.com>
2019-11-08 17:51:33 +0800

    系统配置添加关于

--- app/Modules/Backend/Resources/Views/system/config.blade.php
+++ app/Modules/Backend/Resources/Views/system/config.blade.php
@@ -3,49 +3,62 @@
     <fieldset class="layui-elem-field layui-field-title" style="margin-top: 20px;">
         <legend>系统设置</legend>
     </fieldset>
-    <div class="row">
-        <div class="layui-col-xs6">
-            <form class="layui-form" lay-filter="system_config">
-                <div class="layui-form-item">
-                    <label class="layui-form-label">博客标题</label>
-                    <div class="layui-input-block">
-                        <input type="text" name="title" placeholder="" class="layui-input" value="{{$all_configs['title'] or ''}}">
-                    </div>
-                </div>
-                <div class="layui-form-item">
-                    <label class="layui-form-label">关键字</label>
-                    <div class="layui-input-block">
-                        <input type="text" name="keywords" placeholder="多个关键字以英文逗号分隔" class="layui-input" value="{{$all_configs['keywords'] or ''}}">
-                    </div>
-                </div>
-                <div class="layui-form-item layui-form-text">
-                    <label class="layui-form-label">描述</label>
-                    <div class="layui-input-block">
-                        <textarea name="desc" placeholder="" class="layui-textarea">{{$all_configs['desc'] or ''}}</textarea>
-                    </div>
-                </div>
-                <div class="layui-form-item">
-                    <label class="layui-form-label">ICP备案号</label>
-                    <div class="layui-input-block">
-                        <input type="text" name="icp" placeholder="" class="layui-input" value="{{$all_configs['icp'] or ''}}">
-                    </div>
-                </div>
-                <div class="layui-form-item">
-                    <div class="layui-input-block">
-                        <button type="button" class="layui-btn" lay-submit lay-filter="system_config">提交</button>
-                        <button type="reset" class="layui-btn layui-btn-primary">重置</button>
-                    </div>
-                </div>
-            </form>
+    <form class="layui-form" lay-filter="system_config" style="padding-left: 20px;padding-right: 20px;">
+        <div class="layui-form-item">
+            <label class="layui-form-label">博客标题</label>
+            <div class="layui-input-block">
+                <input type="text" name="title" placeholder="" class="layui-input" value="{{$all_configs['title'] or ''}}">
+            </div>
         </div>
-    </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">关键字</label>
+            <div class="layui-input-block">
+                <input type="text" name="keywords" placeholder="多个关键字以英文逗号分隔" class="layui-input" value="{{$all_configs['keywords'] or ''}}">
+            </div>
+        </div>
+        <div class="layui-form-item layui-form-text">
+            <label class="layui-form-label">描述</label>
+            <div class="layui-input-block">
+                <textarea name="desc" placeholder="" class="layui-textarea">{{$all_configs['desc'] or ''}}</textarea>
+            </div>
+        </div>
+        <div class="layui-form-item layui-form-text">
+            <label class="layui-form-label">关于</label>
+            <div class="layui-input-block">
+                <textarea name="about" placeholder="" class="layui-textarea">{{$all_configs['about'] or ''}}</textarea>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">ICP备案号</label>
+            <div class="layui-input-block">
+                <input type="text" name="icp" placeholder="" class="layui-input" value="{{$all_configs['icp'] or ''}}">
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <div class="layui-input-block">
+                <button type="button" class="layui-btn" lay-submit lay-filter="system_config">提交</button>
+                <button type="reset" class="layui-btn layui-btn-primary">重置</button>
+            </div>
+        </div>
+    </form>
 @endsection
 @section('scripts')
     <script>
+        tinymce.init({
+            selector: 'textarea[name=about]',
+            language:'zh_CN',
+            skin: 'oxide',
+            plugins: 'lists,advlist image codesample link paste',
+            toolbar:'bold italic underline strikethrough alignleft aligncenter alignright alignjustify forecolor backcolor styleselect formatselect fontselect fontsizeselect bullist numlist outdent indent blockquote undo redo removeformat subscript superscript link image codesample',
+            statusbar: false,
+            height: 500
+        });
+
         layui.use(['form'], function () {
             var form = layui.form;
 
             form.on('submit(system_config)', function(data){
+                data.field.about = tinyMCE.activeEditor.getContent();
                 var load_index = layer.load();
                 $.ajax({
                     method: "post",

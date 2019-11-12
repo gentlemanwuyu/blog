f6d302de9427488b3e3dac283e526f9da3a03e26
WuYu <yu.wu@patpat.com>
2019-11-12 21:03:10 +0800

    设置最小高度

--- app/Modules/Frontend/Resources/Views/layouts/default.blade.php
+++ app/Modules/Frontend/Resources/Views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend::layouts.template')
 @section('body')
     @include('frontend::layouts.header')
-    <div class="layui-container">
+    <div class="layui-container" style="min-height: 800px;">
         <div class="layui-row layui-col-space15 main">
             <div class="layui-col-md9 layui-col-lg9">
                 @yield('content')

4eb09afb0cbcc18ab05ce1b8817c7b7fa2e16122
WuYu <yu.wu@patpat.com>
2019-11-22 15:11:03 +0800

    fix

--- app/Modules/Backend/Resources/Views/layouts/template.blade.php
+++ app/Modules/Backend/Resources/Views/layouts/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="csrf-token" content="{!! csrf_token() !!}">
-    <title>Woozee后台管理</title>
+    <title>博客后台管理</title>
     <link rel="stylesheet" href="{{asset('/assets/jstree/dist/themes/default/style.min.css')}}" media="all">
     <link rel="stylesheet" href="{{asset('/assets/fancybox/dist/jquery.fancybox.min.css')}}" media="all">
     <link rel="stylesheet" href="{{asset('/assets/layui-src/dist/css/layui.css')}}" media="all">

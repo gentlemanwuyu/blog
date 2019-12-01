b6fe522f95ba2fb4007644601ad7ca14e40fcda7
wuyu <492444775@qq.com>
2019-12-01 10:55:57 +0800

    fix

--- app/Modules/Frontend/Resources/Views/layouts/footer.blade.php
+++ app/Modules/Frontend/Resources/Views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="layui-col-md12 t-copy">
         <span class="layui-breadcrumb">
             <span>Copyright &copy; {{$year_interval}} <a href="/">Woozee</a></span>
-            <span>Theme by <a href="https://www.echo.so" target="_blank">Echo</a></span>
+            <span>Theme by <a href="https://www.echo.so" rel="external nofollow" target="_blank">Echo</a></span>
         </span>
     </div>
 </div>

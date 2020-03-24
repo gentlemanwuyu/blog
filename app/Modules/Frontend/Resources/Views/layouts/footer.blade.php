01c294742bd7364e6b520a989d1eabb389be22e9
WuYu <yu.wu@patpat.com>
2020-03-24 15:06:22 +0800

    备案号

--- app/Modules/Frontend/Resources/Views/layouts/footer.blade.php
+++ app/Modules/Frontend/Resources/Views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="layui-col-md12 t-copy">
         <span class="layui-breadcrumb">
             <span>Copyright &copy; {{$year_interval}} <a href="/">Woozee</a></span>
-            <span>Theme by <a href="https://www.echo.so" rel="external nofollow" target="_blank">Echo</a></span>
+            <span>备案号：<a href="http://beian.miit.gov.cn" rel="external nofollow" target="_blank">粤ICP备19158101号-1</a></span>
         </span>
     </div>
 </div>

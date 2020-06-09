8a00990f64963f5ebd2bedb93a8b800f95946d1c
WuYu <yu.wu@patpat.com>
2020-06-09 14:11:58 +0800

    修复php升级到7.2时的报错

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,11 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
+if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
+//    error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+     error_reporting(E_ALL ^ E_WARNING);
+}
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
